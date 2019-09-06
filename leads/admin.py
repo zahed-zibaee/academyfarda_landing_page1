db522edc43fc88c9c220ac02d414efd4c42c656c
zahed zibaee <zahed.zibaee2@gmail.com>
2019-09-06 12:37:16 +0430

    i dont remember the changes

--- leads/admin.py
+++ leads/admin.py
@@ -2,11 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import Lead, Token
+from models import Lead, Token, Comment
 import csv
 from django.http import HttpResponse
 from django.utils.encoding import smart_str
 from django.contrib.admin import DateFieldListFilter
+
 
 # Register your models here.
 admin.site.register(Token)
@@ -19,11 +20,19 @@ class Lead_admin(admin.ModelAdmin):
     search_fields = ('phone_number', 'name_and_family', 'discription')
     ########################################
     #calumn value on Lead
-    list_display = ['id','name_and_family','phone_number','led_time','register_status',]
+    list_display = ['id','name_and_family','phone_number','led_time','register_status','discription']
+    ########################################
+    #calumn value on Lead get 
+    list_display_links = ['name_and_family',]
+    ########################################
+    #make editable 
+    list_editable = ['register_status',]
     ########################################
     #to filter by date 
     list_filter = (
         ('led_time', DateFieldListFilter,),
+        'register_status',
+        'token',
     )
     ########################################
     #for export as csv
@@ -35,10 +44,10 @@ class Lead_admin(admin.ModelAdmin):
         response.write(u'\ufeff'.encode('utf8'))
         writer.writerow([
             smart_str(u"ID"),
+            smart_str(u"Token"),
             smart_str(u"Name"),
             smart_str(u"Phone"),
             smart_str(u"Gender"),
-            smart_str(u"Email"),
             smart_str(u"Led Time"),
             smart_str(u"Registered"),
             smart_str(u"Discription"),
@@ -46,10 +55,10 @@ class Lead_admin(admin.ModelAdmin):
         for obj in queryset:
             writer.writerow([
                 smart_str(obj.id),
+                smart_str(obj.token),
                 smart_str(obj.name_and_family),
                 smart_str(obj.phone_number),
                 smart_str(obj.gender),
-                smart_str(obj.email),
                 smart_str(obj.led_time),
                 smart_str(obj.register_status),
                 smart_str(obj.discription),
@@ -60,3 +69,8 @@ class Lead_admin(admin.ModelAdmin):
     actions = ["csv_export"]
     ####################################
     #TODO: date range
+
+@admin.register(Comment)
+class Comment_admin(admin.ModelAdmin):
+    #calumn value on Lead
+    list_display = ['id','author','created_date','post','approved_comment']
--- leads/models.py
+++ leads/models.py
@@ -3,11 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 from datetime import datetime  
-from django.contrib.auth.models import User  
+from django.contrib.auth.models import User
 
+##########
+# Set everything to UTF-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+##########
+   
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=64)
+    def __unicode__(self):
+        return str(self.user) + "_Token"
 
-# Create your models here.
 class Lead(models.Model):
+    token = models.ForeignKey(Token, on_delete=models.SET_NULL, related_name='comments', unique=False, blank = True, null = True, )
     name_and_family = models.CharField(max_length=500, null=False, default='No Name', blank=False)
     GENDER_CHOICES = (
         ('M', 'Male'),
@@ -25,10 +37,13 @@ class Lead(models.Model):
     discription = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
-        return "{} ----- {}".format(self.phone_number, self.name_and_family )
-    
-class Token(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    token = models.CharField(max_length=64)
+        return "{} ----- {}".format(self.phone_number, self.name_and_family)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Lead, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    text = models.TextField(blank=True, null=True)
+    created_date = models.DateTimeField(default=datetime.now)
+    approved_comment = models.BooleanField(default=True)
     def __unicode__(self):
-        return str(self.user) + "_Token"
+        return str(self.author) + ":" + str(self.id) 
