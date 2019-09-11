34697bf245b600a909c5bcf399c9c95cc948d537
zahed zibaee <zahed.zibaee2@gmail.com>
2019-09-11 20:34:26 +0430

    phone number validation

--- leads/models.py
+++ leads/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from datetime import datetime  
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
    
 class Token(models.Model):
@@ -20,7 +21,8 @@ class Lead(models.Model):
         ('F', 'Female'),
     )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M', null=True)
-    phone_number = models.CharField(max_length=15, null=False, default='09000000000', unique=True, blank=False)
+    phone_regex = RegexValidator(regex=r'^[0-9].{6,15}$', message="Phone number must be entered in the format: '09999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, null=False, default='09000000000', unique=True, blank=False)
     R_STAT = (
         ('K', 'OK'),
         ('C', 'Cancel'),
