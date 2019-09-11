cf8b75bfb498fe06f9885b3f572b9a63aca3f1f6
zahed zibaee <zahed.zibaee2@gmail.com>
2019-09-11 23:51:08 +0430

    admin page improvements

--- leads/admin.py
+++ leads/admin.py
@@ -20,18 +20,38 @@ class Lead_admin(admin.ModelAdmin):
     #search field
     search_fields = ('phone_number', 'name_and_family', 'description')
     #calumn value on Lead
-    list_display = ['id','name_and_family','phone_number','led_time', \
+    list_display = ['id','name_and_family','phone_number','gender','led_time', \
         'token','register_status','description']
     #calumn value on Lead get 
     list_display_links = ['name_and_family',]
     #make editable 
-    list_editable = ['register_status',]
+    list_editable = ['register_status','gender',]
     #to filter by date 
     list_filter = (
         ('led_time', DateFieldListFilter,),
         'register_status',
         'token',
     )
+    #define actions
+    actions = ['csv_export','delete_selected']
+    #marketing users have no actions permitions
+    def get_actions(self, request):
+        actions = super(Lead_admin, self).get_actions(request)
+        if request.user.groups.filter(name__in=['marketing']).exists():
+            del actions['csv_export']
+            del actions['delete_selected']
+        return actions
+    #marketing users can't change name_and_family, phone_number and description
+    def save_model(self, request, obj, form, change):
+        if request.user.groups.filter(name__in=['marketing']).exists():
+            if 'name_and_family' not in form.changed_data  \
+                    and 'phone_number' not in form.changed_data  \
+                    and 'description' not in form.changed_data:
+                super(Lead_admin, self).save_model(request, obj, form, change)
+            else:
+                pass
+        else:
+            super(Lead_admin, self).save_model(request, obj, form, change)
     #for export as csv
     def csv_export(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
@@ -70,7 +90,7 @@ class Lead_admin(admin.ModelAdmin):
 @admin.register(Comment)
 class Comment_admin(admin.ModelAdmin):
     #calumn value on Lead
-    list_display = ['id','author','created_date','post','approved_comment']
+    list_display = ['id','author','post','created_date','text','approved_comment']
     #calumn value on Lead get 
     list_display_links = ['author',]
     #make editable
@@ -78,13 +98,24 @@ class Comment_admin(admin.ModelAdmin):
     #limit show up content
     list_per_page = 50
     #search field
-    search_fields = ('post','approved_comment',)
+    search_fields = ('post__id',"post__phone_number",'text')
     #to filter by date 
     list_filter = (
         ('created_date', DateFieldListFilter,),
         'author',
+        'approved_comment',
     )
-    #to authenricate author as logedin admin user
+    #marketing users can't change data
     def save_model(self, request, obj, form, change):
-        obj.author = request.user
-        super(Comment_admin, self).save_model(request, obj, form, change)
+        if request.user.groups.filter(name__in=['marketing']).exists():
+            pass
+        else:
+            super(Comment_admin, self).save_model(request, obj, form, change)
+    #define actions
+    actions = ['delete_selected']
+    #marketing users have no actions permitions
+    def get_actions(self, request):
+        actions = super(Comment_admin, self).get_actions(request)
+        if request.user.groups.filter(name__in=['marketing']).exists():
+            del actions['delete_selected']
+        return actions
