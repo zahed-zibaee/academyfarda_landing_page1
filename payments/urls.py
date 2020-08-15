e3e8b84f463a0c16e9f56385f7c51491afc76b53
zahed zibaee <zahed.zibaee2@gmail.com>
2020-08-15 23:19:48 +0430

    bug fixes

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^request/$', views.send_request, name='request'),
-    url(r'^verify/$', views.verify , name='verify'),
+    url(r'^request/?$', views.send_request, name='request'),
+    url(r'^verify/?$', views.verify , name='verify'),
 ]
