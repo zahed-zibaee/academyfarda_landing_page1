4d81482c9aef001f11abcf2e7faf3ee151880c14
zahed-zibaee <zahed.zibaee2@gmail.com>
2020-08-22 20:32:13 +0430

    lookup changes

--- SMS/views.py
+++ SMS/views.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.shortcuts import redirect, render, get_object_or_404
-from .models import Verify, Sent
-from payments.models import Discount
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import redirect, render
+from .models import Sent, Verify
 from persiantools import digits
 from datetime import datetime,timedelta
 from random import randint
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
@@ -24,23 +22,16 @@ def lookup(request):
         ip = request.POST['ip']
         phone_fa = digits.ar_to_fa(request.POST['phone'])
         phone_en = digits.fa_to_en(phone_fa)
-        # last 24h same ip
+        #esist in last 24h same ip
         if Verify.objects.filter(ip = ip, sent__created_date__lt = datetime.now()\
-            , sent__created_date__gt = datetime.now() + timedelta(days=-1)).exists():
-            if len(Verify.objects.filter(ip = ip, sent__created_date__lt = datetime.now()\
+            , sent__created_date__gt = datetime.now() + timedelta(days=-1)).exists()\
+            and len(Verify.objects.filter(ip = ip, sent__created_date__lt = datetime.now()\
             , sent__created_date__gt = datetime.now() + timedelta(days=-1))) > 9:
                 return JsonResponse({'status':'3','id':'-1'})
-            elif Verify.objects.filter(sent__receptor = phone_en, sent__created_date__lt = datetime.now()\
-            , sent__created_date__gt = datetime.now() + timedelta(minutes=-1)).exists():
+        #exist in last min
+        elif Verify.objects.filter(sent__receptor = phone_en, sent__created_date__lt = datetime.now()\
+            , sent__created_date__gt = datetime.now() + timedelta(minutes=-2)).exists():
                 return JsonResponse({'status':'2','id':'-1'})
-            else:
-                obj = Verify.objects.create(sent = Sent.objects.create( receptor = phone_en)\
-                , ip = ip, token1=''.join(["{}".format(randint(0, 9)) for num in range(0, 3)]),\
-                    token2 = ''.join(["{}".format(randint(0, 9)) for num in range(0, 3)]) )
-                obj.expiration_time = datetime.now() + timedelta(minutes=5)
-                obj.save()
-                obj.send()
-                return JsonResponse({'status':'0','id':obj.id})
         else:
             obj = Verify.objects.create(sent = Sent.objects.create( receptor = phone_en)\
             , ip = ip, token1=''.join(["{}".format(randint(0, 9)) for num in range(0, 3)]),\
